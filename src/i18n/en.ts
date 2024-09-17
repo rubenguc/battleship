23e27516b9b2640f0b513920848ed630be64f378
Ruben <rubdeivis@gmail.com>
2024-09-17 18:58:35 -0400

    feat: Add i18n

---
+++ src/i18n/en.ts
@@ -0,0 +1,9 @@
+export default {
+  home: {
+    login_with_google: "Login with google",
+    login_with_github: "Login with github",
+    create_game: "Create game",
+    join_game: "Join game",
+    logout: "Log out",
+  },
+};
